a9157d30c013303a4ceaa30bb16bf53d1550b9b6
v005480 <constantin.gonciulea@gmail.com>
2013-10-13 20:40:32 -0400

    cleanup

--- src/main/java/org/resthub/training/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.resthub.training.repository;
-
-import org.resthub.training.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- src/main/java/org/resthub/training/service/NotificationService.java
+++
@@ -1,6 +0,0 @@
-package org.resthub.training.service;
-
-public interface NotificationService {
-
-    void send(String email, String message);
-}
